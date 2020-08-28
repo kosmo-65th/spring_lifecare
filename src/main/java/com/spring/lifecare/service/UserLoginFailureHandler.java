5a8ef5b39d156e4845c1f0f10e20a97d7c74ac0e
swhan9404 <swhan9404@naver.com>
2020-08-28 13:19:01 +0900

    쓰레기값

--- src/main/java/com/spring/lifecare/service/UserLoginFailureHandler.java
+++ src/main/java/com/spring/lifecare/service/UserLoginFailureHandler.java
@@ -19,6 +19,8 @@ public class UserLoginFailureHandler implements AuthenticationFailureHandler {
 		
 		request.setAttribute("errMsg", "아이디 또는 비밀번호가 일치하지 않습니다.");
 		
+		System.out.print("아이디 또는 비밀번호가 일치하지 않습니다.");
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/user/login.jsp");
 		dispatcher.forward(request, response);
 	}
