5dbe228d112b627c838a5da15dd87c40df55e04c
swhan9404 <swhan9404@naver.com>
2020-09-02 14:41:15 +0900

    git Ignore

--- src/main/java/com/spring/lifecare/controller/SwhController.java
+++ src/main/java/com/spring/lifecare/controller/SwhController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SwhController {
+//
 	//메인페이지
 	@RequestMapping(value= {"/","/guest/main","/main"})
 	public String main(Model model) {
