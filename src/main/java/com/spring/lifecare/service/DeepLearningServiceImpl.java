55cd7d6bf84502d297e825c327c95bbfb7a2dda9
swhan9404 <swhan9404@naver.com>
2020-10-07 14:20:58 +0900

    test

--- src/main/java/com/spring/lifecare/service/DeepLearningServiceImpl.java
+++ src/main/java/com/spring/lifecare/service/DeepLearningServiceImpl.java
@@ -28,59 +28,12 @@ public class DeepLearningServiceImpl implements DeepLearningService{
 
 	@Override
 	public Map<String, String> covidTest(MultipartHttpServletRequest req, Model model) throws IOException {
-		String modelfile = new ClassPathResource("../../../../../webapp/resources/deeplearningModel/mobileNetV2model6.h5").getFile().getPath();
-		ComputationGraph Deepmodel = null;
-		try {
-			Deepmodel = KerasModelImport.importKerasModelAndWeights(modelfile);
-		} catch (UnsupportedKerasConfigurationException | InvalidKerasConfigurationException e) {
-			System.out.println("error : "+e.getMessage());
-		}
-		
-		// 사진 파일 경로
-		//String saveDir = req.getSession().getServletContext().getRealPath("/resources/images/");  // 업로드할 파일이 위치하게될 실제 경로
-		String projectRoot = System.getProperty("user.dir");
-		System.out.println(projectRoot);
-		
 		
 		return null;
 	}
 
 	@Override
 	public Map<String, String> covidTestTest(HttpServletRequest req) {
-		/*
-		String rootDir = DeepLearningServiceImpl.class.getResource(".").getPath();
-		System.out.println("RootDir : "+rootDir);  // /C:/install/apache-tomcat-8.5.57/wtpwebapps/spring_lifecare/WEB-INF/classes/com/spring/lifecare/service/
-		
-		rootDir = rootDir.replace("WEB-INF/classes/com/spring/lifecare/service/", "");
-		System.out.println("reRootDir : "+rootDir);  // C:/install/apache-tomcat-8.5.57/wtpwebapps/spring_lifecare/
-		
-		rootDir += "resources/deeplearningModel/"; // C:/install/apache-tomcat-8.5.57/wtpwebapps/spring_lifecare/resources/deeplearningModel/
-		rootDir = rootDir.substring(1);
-		System.out.println("rRReRootDir : "+rootDir);
-		*/
-		//모델 직렬화
-	
-		String modelfile="";
-		String weightfile ="";
-		try {
-			modelfile = new ClassPathResource("deeplearningModel/mobileNetV2model7_complete.h5").getFile().getPath();
-			weightfile = new ClassPathResource("deeplearningModel/mobileNetV2model7_weight.h5").getFile().getPath();
-		} catch (IOException e1) {
-			System.out.println("service-covidTestTest IOException");
-		}
-		
-		try {
-			ComputationGraph graph = KerasModelImport.importKerasModelAndWeights(modelfile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedKerasConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidKerasConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		return null;
 	}
 
